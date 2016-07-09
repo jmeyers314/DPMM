eac6017ed3f9dc033f53ac2286875e86326ab72b
jmeyers314 <jmeyers314@gmail.com>
2016-07-08 18:52:25 -0700

    Add NormInvChi2 broadcast tests.

--- tests/test_broadcast.py
+++ tests/test_broadcast.py
@@ -255,6 +255,126 @@ def test_NormInvGamma():
     assert arr.dtype == float
 
 
+@timer
+def test_NormInvChi2():
+    """Test broadcasting rules for NormInvChi2 prior."""
+
+    # Test sample() method:
+    prior = dpmm.NormInvChi2(1.0, 1.0, 1.0, 1.0)
+    arr = prior.sample()
+    assert isinstance(arr, np.void)
+    assert arr.dtype == prior.model_dtype
+
+    arr = prior.sample(size=1)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (1,)
+    assert arr.dtype == prior.model_dtype
+
+    arr = prior.sample(size=(1,))
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (1,)
+    assert arr.dtype == prior.model_dtype
+
+    arr = prior.sample(size=10)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (10,)
+    assert arr.dtype == prior.model_dtype
+
+    arr = prior.sample(size=(10, 20))
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (10, 20)
+    assert arr.dtype == prior.model_dtype
+
+    # Test like1() method:
+    prior = dpmm.NormInvChi2(1.0, 1.0, 1.0, 1.0)
+    x = 1.0
+    mu = 1.0
+    var = 1.0
+    arr = prior.like1(x, mu, var)
+    assert isinstance(arr, float)
+
+    x = np.array([1.0])
+    mu = 1.0
+    var = 1.0
+    arr = prior.like1(x, mu, var)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (1,)
+    assert arr.dtype == float
+
+    x = np.array([1.0, 2.0, 3.0, 4.0])
+    mu = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    var = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    arr = prior.like1(x[:, np.newaxis, np.newaxis], mu, var)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (4, 2, 3)
+    assert arr.dtype == float
+
+    theta = np.zeros((2, 3), dtype=prior.model_dtype)
+    theta['mu'] = mu
+    theta['var'] = var
+    arr = prior.like1(x[:, np.newaxis, np.newaxis], theta)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (4, 2, 3)
+    assert arr.dtype == float
+
+    arr = prior.like1(x, mu[:, :, np.newaxis], var[:, :, np.newaxis])
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (2, 3, 4)
+    assert arr.dtype == float
+
+    arr = prior.like1(x, theta[:, :, np.newaxis])
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (2, 3, 4)
+    assert arr.dtype == float
+
+    # Test __call__() method:
+    prior = dpmm.NormInvChi2(1.0, 1.0, 1.0, 1.0)
+    mu = 1.0
+    var = 1.0
+    arr = prior(mu, var)
+    assert isinstance(arr, float)
+
+    mu = np.array([1.0])
+    arr = prior(mu, var)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (1,)
+    assert arr.dtype == float
+
+    mu = np.array([1.0, 2.0])
+    var = np.array([10.0, 11.0, 12.0])
+    arr = prior(mu[:, np.newaxis], var)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (2, 3)
+    assert arr.dtype == float
+
+    # Should _post_params method do any broadcasting?
+
+    # Test pred method():
+    prior = dpmm.NormInvChi2(1.0, 1.0, 1.0, 1.0)
+    x = 1.0
+    arr = prior.pred(x)
+    assert isinstance(arr, float)
+
+    x = np.array([1.0])
+    arr = prior.pred(x)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (1,)
+    assert arr.dtype == float
+
+    x = np.array([1.0, 2.0])
+    arr = prior.pred(x)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (2,)
+    assert arr.dtype == float
+
+    x = np.array([[1.0, 2.0], [3.0, 4.0]])
+    arr = prior.pred(x)
+    assert isinstance(arr, np.ndarray)
+    assert arr.shape == (2, 2)
+    assert arr.dtype == float
+
+
 if __name__ == '__main__':
     test_InvGamma()
     test_NormInvGamma()
+    test_NormInvChi2()
